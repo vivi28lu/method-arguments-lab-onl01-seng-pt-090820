ad5e574d6d1a22a15da638a21ea2811ec24dfe63
Viviana Lugo <vivi28lu@gmail.com>
2020-09-11 16:52:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,6 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction ("Josh")
+introduction "Josh"
 
 introduction_with_language "Viviana", "Java"
